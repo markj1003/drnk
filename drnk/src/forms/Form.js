edd7054690e1d88cd08788ef57d3258a601feae7
jxr <jxr@corefiling.com>
2023-03-10 19:14:27 +0000

    set redux state when logging in

--- drnk/src/forms/Form.js
+++ drnk/src/forms/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,16 +6,31 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default function GenericForm(props) {
+    const [userInput, setUserInput] = useState({});
+    const getValue = (fieldName) => {
+        return fieldName in userInput ? userInput[fieldName] : "";
+    }
+    const handleChange = (ev) => {
+        setUserInput({...userInput, [ev.target.name]: ev.target.value});
+    }
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        props.onSubmit(ev, userInput);
+        setUserInput({});
+    }
+
     return (
         <Container>
             <Row className="justify-content-center">
                 <Col md={{span: 10}}>
-                    <Form onSubmit={props.onSubmit}>
+                    <Form onSubmit={onSubmit}>
                         <h1 className="text-primary">{props.prompt}</h1>
                         {props.inputFields.map((inputField)=>(
                             <Form.Group className="mb-4" key={inputField}>
                                 <Form.Label>{inputField}</Form.Label>
-                                <Form.Control name={inputField} placeholder={"Enter " +  inputField} />
+                                <Form.Control name={inputField} placeholder={"Enter " +  inputField}
+                                value={getValue(inputField)} onChange={handleChange}/>
                             </Form.Group>  ))}
                         {/* it's a bit hacky to just repeat the header prompt as the submit button */}
                         <Button type="submit" className="btn btn-primary btn-block mb-2">{props.prompt}</Button>
--- drnk/src/forms/login/LoginForms.js
+++ drnk/src/forms/login/LoginForms.js
@@ -4,11 +4,21 @@ import Col from "react-bootstrap/Col";
 import GenericForm from "../Form";
 import Card from "react-bootstrap/Card";
 import {Link} from "react-router-dom";
+import {log_in, new_account} from "../../interface";
+import store from "../../store";
+import {setLoggedIn} from "../../storeSlices/loginSlice";
 
 function LoginForm() {
+    const onSubmit = async (ev, userInput) => {
+        let token = await log_in(userInput["Username"], userInput["Password"]);
+        if (token) {
+            store.dispatch(setLoggedIn(token))
+            console.log("logged in");
+        }
+    }
     return (
         <React.Fragment>
-            <GenericForm prompt={"Log in"} inputFields={["Username", "Password"]}/>
+            <GenericForm prompt={"Log in"} inputFields={["Username", "Password"]} onSubmit={onSubmit}/>
             <Row className="justify-content-center">
                 <Col>
                     <Link to={"/auth/reset"} className='link-primary'>Forgot password?</Link>
@@ -24,6 +34,17 @@ function LoginForm() {
 }
 
 function SignupForm() {
+    const onSubmit = (ev, userInputs) => {
+        if (userInputs["Password"] != userInputs["Confirm Password"]) {
+            console.log("passwords don't match");
+            return;
+        }
+        let token = new_account(userInputs["Username"], userInputs["Password"]);
+        if (token) {
+            store.dispatch(setLoggedIn(token))
+            console.log("signed up")
+        }
+    }
     return (
         <React.Fragment>
             <GenericForm prompt={"Sign up"} inputFields={["Username", "Email", "Password", "Confirm Password"]} />
